245004a5ebf7df8337a676466f23e2a8fee016e6
Max Mercuriev <max@yogi.pw>
2024-10-28 19:47:14 +0500

    fetch Offer primitive data from newly deployed contract

--- src/market.ts
+++ src/market.ts
@@ -1,42 +1,43 @@
-import {
-  DealCreated as DealCreatedEvent,
-  Initialized as InitializedEvent,
-  OfferCreated as OfferCreatedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/Market/Market"
-import {
-  Offer,
-  RepToken
-} from "../generated/schema"
-
-export function handleDealCreated(event: DealCreatedEvent): void {
-  let entity = new DealCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.offerOwner = event.params.offerOwner
-  entity.taker = event.params.taker
-  entity.offer = event.params.offer
-  entity.deal = event.params.deal
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import {OfferCreated as OfferCreatedEvent} from "../generated/Market/Market"
+import {Offer as OfferEntity} from "../generated/schema"
+import {Offer as OfferContract} from "../generated/Market/Offer"
+import {Address} from "@graphprotocol/graph-ts"
 
 export function handleOfferCreated(event: OfferCreatedEvent): void {
-  let offer = new Offer(event.params.offer.toHex())
+  let offer = new OfferEntity(event.params.offer.toHex())
   offer.owner = event.params.owner
-  offer.isSell = event.params.isSell
-  offer.token = event.params.token.toString()
-  offer.fiat = event.params.fiat.toString()
-  offer.method = event.params.method.toString()
-  offer.rate = event.params.rate.toI32()
-  offer.minLimit = event.params.minLimit.toI32()
-  offer.maxLimit = event.params.maxLimit.toI32()
-  offer.terms = event.params.terms.toString()
+  offer.token = event.params.token.toHexString()
+  offer.fiat = event.params.fiat.toHexString()
+
+  // Fetch data from the newly created Offer contract
+  let offerContract = OfferContract.bind(event.params.offer as Address)
+
+  let isSellResult = offerContract.try_isSell()
+  if (!isSellResult.reverted) {
+    offer.isSell = isSellResult.value
+  }
+
+  let methodResult = offerContract.try_method()
+  if (!methodResult.reverted) {
+    offer.method = methodResult.value
+  }
+
+  let rateResult = offerContract.try_rate()
+  if (!rateResult.reverted) {
+    offer.rate = rateResult.value
+  }
+
+  let limitsResult = offerContract.try_limits()
+  if (!limitsResult.reverted) {
+    offer.minLimit = limitsResult.value.getMin()
+    offer.maxLimit = limitsResult.value.getMax()
+  }
+
+  let termsResult = offerContract.try_terms()
+  if (!termsResult.reverted) {
+    offer.terms = termsResult.value
+  }
+
   offer.blockNumber = event.block.number
   offer.blockTimestamp = event.block.timestamp
   offer.transactionHash = event.transaction.hash
